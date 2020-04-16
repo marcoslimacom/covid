3f14cd1c920dd8e5861c13214d9cd158bf5ed52f
Marcos Lima <marcoslima@marcoslima.com>
2020-04-16 16:26:27 -0700

    Added hash

--- app/app.js
+++ app/app.js
@@ -3,5 +3,6 @@ const update = require("./update-v2");
 
 const dataPath = path.join(__dirname, "csv");
 const outputPath = path.join(__dirname, "../docs", "full-data.json");
+const outputHashPath = path.join(__dirname, "../docs", "full-data-hash.json");
 
-update(dataPath, outputPath);
+update(dataPath, outputPath, outputHashPath);
--- app/update-v2.js
+++ app/update-v2.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 const csvtojson = require("csvtojson");
+const { v4: uuidv4 } = require("uuid");
 
 const FILENAME_COVID_DATA = "owid-covid-data.csv";
 
-async function update(dataPath, outputPath) {
+async function update(dataPath, outputPath, outputHashPath) {
   var filePath = path.resolve(dataPath, FILENAME_COVID_DATA);
   let data = await csvtojson().fromFile(filePath);
 
@@ -30,6 +31,7 @@ async function update(dataPath, outputPath) {
   });
 
   fs.writeFileSync(outputPath, JSON.stringify(data));
+  fs.writeFileSync(outputHashPath, JSON.stringify({ hash: uuidv4() }));
 }
 
 module.exports = update;
