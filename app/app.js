5c41af63417808a09d8379872405e5a8f772c23f
Marcos Lima <marcoslima@marcoslima.com>
2020-04-18 23:11:51 -0700

    Added v3 structure

--- app/app.js
+++ app/app.js
@@ -1,8 +1,15 @@
 const path = require("path");
-const update = require("./update-v2");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const update2 = require("./update-v2");
+const update3 = require("./update-v3");
 
+const outputHashPath = path.join(__dirname, "../docs", "data-hash.json");
 const dataPath = path.join(__dirname, "csv");
 const outputPath = path.join(__dirname, "../docs", "full-data.json");
-const outputHashPath = path.join(__dirname, "../docs", "full-data-hash.json");
+const outputPathV3 = path.join(__dirname, "../docs", "full-data-v3.json");
 
-update(dataPath, outputPath, outputHashPath);
+update2(dataPath, outputPath);
+update3(dataPath, outputPathV3);
+
+fs.writeFileSync(outputHashPath, JSON.stringify({ hash: uuidv4() }));
